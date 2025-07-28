97dd213b4e3a49e56281b857e1a993a47d11f1f4
007Irish <vermagirish1620@gmail.com>
2025-07-28 13:43:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print("hello world")
+print("hello chintu")
+print("i am great")
+print("what about you?")
+print(57)
 
-abs(5)
+# if(a>3):
+#     hjrjefif  #this statement is inside the if statement
+# gjikfm #this statement is outside the if statement
+
+# the space before the first statement is called indentation. It is used to organise the code. It is a way to tell the python program weather we are inside the block or not.
