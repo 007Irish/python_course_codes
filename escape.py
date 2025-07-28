81a539bc3b87f05a34b684771a07fa61cfa619a2
007Irish <vermagirish1620@gmail.com>
2025-07-28 13:46:37 +0530

    Add files via upload

---
+++ escape.py
@@ -0,0 +1,14 @@
+print("Hey how are you\nI am good") #\n is used to move the rest of yhe part to new line.
+
+#\t : tab
+#\\: Backslash
+#\": Double quote
+#\': Single quote
+
+print("Hello World", "Harry" , 5 , sep="" , end=",")
+print("Hello World", "Harry" , 5 , sep="/")
+# print('Hello World', end=", ")
+# print('Girish')
+
+#sep to add something between two objects or arguments.
+# end to add something at the end of the print statement and add the other print statement into it.
