a9ce162f2b23e5ecb17e4b7f8fd411e2513d62ea
007Irish <vermagirish1620@gmail.com>
2025-07-28 13:46:59 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,52 @@
+a = 34
+
+b=2
+
+# Arithmetic Operator
+
+print("a + b =  ", a + b)
+
+print("a - b = ", a - b)
+
+print("a * b = ", a * b)
+
+print("a / b = ", a / b)
+
+print("a % b = ", a % b)
+
+print("a // b = ", a // b)
+
+print("a ** b = ", a ** b)
+
+# Conditional Operators
+
+print(a>4)
+
+print(a<4)
+
+print(a<=4)
+
+print(a>=4)
+
+print(a==4) # Is a equal to 4?
+
+print(a==34) # Is a equal to 34?
+
+print(a!=34) # Is a not equal to 34?
+
+
+c = True
+
+d = False
+
+# Logical Operators 
+print(True and True)
+print (True and False) 
+print (False and True) 
+print (False and False)
+
+print("For Or Operator...")
+print(True or True) 
+print(True or False)
+print(False or True) 
+print (False or False) 
